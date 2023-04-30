301e001bc197cf95fa060e47a13ef66ae2f30791
Shantanu <shantanusut2000@gmail.com>
2023-04-30 17:34:06 +0530

    Random Num

--- React/interview_prep/src/Components/RandomNum.jsx
+++ React/interview_prep/src/Components/RandomNum.jsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 
 const RandomNum = () => {
     const [num, setNum] = useState(0);
+
     const randomNum = () => {
-        const newNum = Math.floor(Math.random());
+        const newNum = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER); // it will genrate a maximum safest random number
         setNum(newNum);
     };
     return (
         <div>
-            <button onClick={randomNum}>{num}</button>
+            <button onClick={randomNum}>Number is :- {num}</button>
         </div>
     );
 };
